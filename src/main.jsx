cd143b7cb8964b183be8ba987082730118eb4aba
Frybberd <frybberd@gmail.com>
2025-04-17 10:13:03 -0400

    Arreglar nombre main.jsx y ruta en index.html

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import YaracuyExperienceDemo from './YaracuyExperienceDemo'
+
+ReactDOM.createRoot(
+  document.getElementById('root')
+).render(
+  <React.StrictMode>
+    <YaracuyExperienceDemo />
+  </React.StrictMode>
+)
